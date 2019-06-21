db28df26495116d76b7b5ba25efd3bc7229d2aa0
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-06-21 10:10:18 -0400

    added duplicate checks and invalid character checks 10:09  6/21 added CSS and Boostrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,154 +1,165 @@
 // list of random words
-// var randomWord = ['awkward','bagpipes','banjo','bungler','croquet','crypt','dwarves','fervid','fishhook','house','gazebo','gypsy','haiku','haphazard','hyphen',
-//     'ivory','jazzy','jiffy','jinx','jukebox', 'kayak','kiosk','klutz', 'memento', 'mystify', 'numbskull', 'ostracize', 'oxygen', 'pajama', 'phlegm','pixel',
-//     'polka','quad','quip','rhythmic','rogue','sphinx','squawk','swivel','toady','twelfth','unzip','waxy','wildebeest','yacht','zealous','zigzag','zippy','zombie',
-//     'dramatic', 'notebook','detail','graceful','carriage','plate','fold','rifle','memory','skillful','vengeful','brief','young','yarn','certain','evasive','deep',
-//     'shiny','lackadaisical','open','obedient','witty','shiver','observation','bird','demonic','answer','aware','comparison','tedious','zealous','greasy','acoustic',
-//     'cars','sincere','valuable','cave','curl','mine','plain','voiceless','defective','cable','mend','crabby','sturdy','knot','use','scissors','tow'];
-  var randomWord = ["scissors"];
-    
-    // number of guesses allowed
-    var guess = 10;
-    // number of wins
-    var wins = 0;
-   
-    // generates a random word form the randon word array
-    var randomNumber = Math.floor(Math.random(randomWord)*randomWord.length);
-    var hiddenWord=(randomWord[randomNumber]);  
-    // Empty arrays to store guess and answer dashes
-    var answerArray = [];
-    var lettersUsed = []; 
-    
-//    Generates a new word and displays dashes,puts the curser in the guess letter box
-    function startGame(){
-        
-        // document.getElementById("letter").focus();
-        
-         for (var i = 0; i < hiddenWord.length; i++) {
-            answerArray[i] = "_";
-        }
-        document.getElementById("answer").innerHTML = answerArray;
-        guess=10;
-        document.getElementById("numofguess").innerHTML = guess + " guesses left";
-        lettersUsed=[]; 
-        
-    };
+var randomWord = ['awkward','bagpipes','banjo','bungler','croquet','crypt','dwarves','fervid','fishhook','house','gazebo','gypsy','haiku','haphazard','hyphen',
+    'ivory','jazzy','jiffy','jinx','jukebox', 'kayak','kiosk','klutz', 'memento', 'mystify', 'numbskull', 'ostracize', 'oxygen', 'pajama', 'phlegm','pixel',
+    'polka','quad','quip','rhythmic','rogue','sphinx','squawk','swivel','toady','twelfth','unzip','waxy','wildebeest','yacht','zealous','zigzag','zippy','zombie',
+    'dramatic', 'notebook','detail','graceful','carriage','plate','fold','rifle','memory','skillful','vengeful','brief','young','yarn','certain','evasive','deep',
+    'shiny','lackadaisical','open','obedient','witty','shiver','observation','bird','demonic','answer','aware','comparison','tedious','zealous','greasy','acoustic',
+    'cars','sincere','valuable','cave','curl','mine','plain','voiceless','defective','cable','mend','crabby','sturdy','knot','use','scissors','tow'];
 
-    function lettersAllowed(allowed){
-        var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        // Iterate through alphabet
-        for (var i = 0; i < alphabet.length; i++){
-            console.log(alphabet[i]);
-            // Checks if input letter is not contained in alphabet
-            if(alphabet[i]===allowed)
-            {   
-                console.log(allowed);
-                
-                return false;
-                
-            }
+// number of guesses allowed
+var guess = 10;
+// number of wins set to 0
+var wins = 0;
 
-        }
-        
-        return true;
+// generates a random word form the randon word array
+var randomNumber = Math.floor(Math.random(randomWord) * randomWord.length);
+var hiddenWord = randomWord[randomNumber];
+// Empty arrays to store guess and answer dashes
+var answerArray = [];
+// Empty array will store users guesses
+var lettersUsed = [];
+//array of allowed keys to press
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+//  Start game, generates a new word and displays dashes
+function startGame() {
+  for (var i = 0; i < hiddenWord.length; i++) {
+    answerArray[i] = "_";
+  }
+  document.getElementById("answer").innerHTML = answerArray;
+  guess = 10;
+  document.getElementById("numofguess").innerHTML = guess + " guesses left";
+  lettersUsed = [];
+}
+//Checks for invalid key characters pressed
+function lettersAllowed(allowed) {
+  // Iterate through alphabet
+  for (var i = 0; i < alphabet.length; i++) {
+    // Checks if input letter is contained in alphabet
+    if (alphabet[i] === allowed) {
+      console.log(allowed);
+      return false;
     }
+  }
 
-    function checkDuplicates(letter, letterArray){
-        
-
-        // Iterate through lettersUsed
-        for (var i = 0; i < letterArray.length; i++){
-
-
-            // Checks if letter is contained in lettersUsed
-            if(letterArray[i].toLocaleUpperCase()===letter.toLocaleUpperCase())
-            {
-            
-                return true;
-                
-            }
-
-        }
-        
-        return false;
-    }
-
-    document.onkeyup = function(event){
-             var inputLetter = event.key;
-             console.log("about to check")
-
-            //check if pressed key is valid letter character
-            if (lettersAllowed(inputLetter)){
-                alert("not a valid key");
-            }else{
-             //Check if inputLetter is in lettersUsed
-             if (checkDuplicates(inputLetter, lettersUsed)){
-
-                alert("you already used "+ inputLetter);
-                 
-             } else {
-                
-                if (hiddenWord.includes(inputLetter)){
-                document.getElementById("match").innerHTML = inputLetter.toUpperCase() + " "+ "found!"
-
-                } else{
-                    document.getElementById("match").innerHTML = inputLetter.toUpperCase() + " "+ "Not found!"
-                    guess--;
-                    document.getElementById("numofguess").innerHTML = guess + " guesses left";
-                        if (guess===0){
-                        alert("GAME OVER")
-                        var again =confirm("Do you want to play again?");
-                            if(again){
-                                location.reload(); 
-                            }
-                            else{
-                                alert("Thanks for playing");
-                            }
-                        }
-                    }
-            
-                     for (var j = 0; j < hiddenWord.length; j++) {
-                         if (hiddenWord[j] === inputLetter) {
-                                answerArray[j] = inputLetter.toUpperCase();
-                            }
-        
-                                document.getElementById("answer").innerHTML = answerArray;
-                        }
-
-                            lettersUsed.push(inputLetter.toUpperCase());   
-                            console.log(lettersUsed)   
-                            document.getElementById("used").innerHTML= "Letters used " + lettersUsed;
-
-                            if (answerArray.join("")===hiddenWord.toLocaleUpperCase()){
-                                wins++;
-                                document.getElementById("numofwins").innerHTML = "Wins " + wins;
-                                document.getElementById("used").innerHTML= "Letters used "; 
-
-                                document.getElementById("answer").innerHTML = answerArray;
-                                guess=10;
-                                document.getElementById("numofguess").innerHTML = guess + " guesses left";
-
-                                alert("the word is " + hiddenWord + " You win");
-
-                                lettersUsed=[]; 
-
-                                startGame();   
-                            }
-                        }
-                    }                 
-         };
-         
-    function showAnswer(){
-        document.getElementById("answer").innerHTML = hiddenWord.toLocaleUpperCase();
-        guess=0;
-        wins=0;
-        document.getElementById("numofguess").innerHTML = guess + " guesses left";
-        document.getElementById("numofwins").innerHTML = "Wins " + wins;
-    };
-
-document.getElementById("showanswer").onclick = function(){
-    showAnswer()
+  return true;
 }
 
-    
+//Checks for duplicate guesses and keeps guesses the same
+function checkDuplicates(letter, letterArray) {
+  // Iterate through lettersUsed
+  for (var i = 0; i < letterArray.length; i++) {
+    // Checks if letter is contained in lettersUsed
+    if (letterArray[i].toLocaleUpperCase() === letter.toLocaleUpperCase()) {
+      return true;
+    }
+  }
 
+  return false;
+}
+
+//event function starts checking for matching letters
+document.onkeyup = function(event) {
+  var inputLetter = event.key;
+  document.getElementById("winner").innerHTML = " ";
+  console.log("about to check");
+
+  //iterate through hidden word to find matches and add them to the answer replacing the dashes.
+  for (var j = 0; j < hiddenWord.length; j++) {
+    if (hiddenWord[j] === inputLetter) {
+      answerArray[j] = inputLetter.toUpperCase();
+    }
+
+    document.getElementById("answer").innerHTML = answerArray;
+  }
+
+  //check if pressed key is valid letter character by calling lettersAllowed function
+  if (lettersAllowed(inputLetter)) {
+    alert("not a valid key");
+  } else {
+    //Check if inputLetter is in lettersUsed by calling the checkDuplicates
+    if (checkDuplicates(inputLetter, lettersUsed)) {
+      alert("you already used " + inputLetter);
+    } else {
+      //if guess matches the hidden word letter found message
+      if (hiddenWord.includes(inputLetter)) {
+        document.getElementById("match").innerHTML =
+          inputLetter.toUpperCase() + " " + "found!";
+      } else {
+        //if guess does not match the hidden word letter not found message and guess down by 1
+        document.getElementById("match").innerHTML =
+          inputLetter.toUpperCase() + " " + "Not found!";
+        guess--;
+        document.getElementById("numofguess").innerHTML =
+          guess + " guesses left";
+
+        //If guesses reaches 0 then the game is over and prompts for a new game
+        if (guess === 0) {
+          alert("GAME OVER");
+          var again = confirm("Do you want to play again?");
+          if (again) {
+            location.reload();
+          } else {
+            alert("Thanks for playing");
+          }
+        }
+      }
+      //Creates an array of letters used and displays them.
+      lettersUsed.push(inputLetter.toUpperCase());
+      document.getElementById("used").innerHTML = "Letters used " + lettersUsed;
+      //End of Game if the answer matches the hidden user wins the game
+      if (answerArray.join("") === hiddenWord.toLocaleUpperCase()) {
+        wins++;
+        document.getElementById("numofwins").innerHTML = "Wins " + wins;
+        document.getElementById("winner").innerHTML = "YOU WIN";
+        document.getElementById("used").innerHTML = " ";
+        document.getElementById("match").innerHTML = " ";
+        lettersUsed = [];
+        startGame();
+      }
+    }
+  }
+};
+
+//If the user gives up this is the option to quit the game
+function showAnswer() {
+  var quit = confirm("Are you sure you want to quit?");
+  if (quit) {
+    document.getElementById("answer").innerHTML = hiddenWord.toLocaleUpperCase();
+    guess = 0;
+    wins = 0;
+    document.getElementById("numofguess").innerHTML = guess + " guesses left";
+    document.getElementById("numofwins").innerHTML = "Wins " + wins;
+  }
+}
+
+document.getElementById("showanswer").onclick = function() {
+  showAnswer();
+};
